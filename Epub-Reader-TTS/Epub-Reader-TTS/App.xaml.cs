cc6d4aec1a1cb24cf2aab92f3f5e814708836cf7
jumah safarty <safartyjumah@gmail.com>
2019-12-04 22:04:10 +0200

    Setting after merging conflict

--- Epub-Reader-TTS/Epub-Reader-TTS/App.xaml.cs
+++ Epub-Reader-TTS/Epub-Reader-TTS/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.CognitiveServices.Speech;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -28,50 +27,13 @@ namespace Epub_Reader_TTS
             // Setup IoC 
             //IoC.Setup();
 
-            Test();//.GetAwaiter().GetResult();
+            //Test();//.GetAwaiter().GetResult();
 
-            //Current.MainWindow = new MainWindow();
+            Current.MainWindow = new MainWindow();
 
-            //Current.MainWindow.Show();
-
-        }
-
-        private async Task Test()
-        {
-
-            var text = @"The experienced publisher misdirects the downhill dragon. When will a suite object? Can the credible ideal nose? When will a dustbin collapse underneath a trained politician?";
-
-            var sc = SpeechConfig.FromSubscription("YourSubscriptionKey", "YourServiceRegion");
-
-
-
-            var ss = new SpeechSynthesizer(sc);
-
-
-            using (var result = await ss.SpeakTextAsync(text))
-            {
-                if (result.Reason == ResultReason.SynthesizingAudioCompleted)
-                {
-                    Debug.WriteLine($"Speech synthesized to speaker for text [{text}]");
-                }
-                else if (result.Reason == ResultReason.Canceled)
-                {
-                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
-                    Debug.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-                    if (cancellation.Reason == CancellationReason.Error)
-                    {
-                        Debug.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                        Debug.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
-                        Debug.WriteLine($"CANCELED: Did you update the subscription info?");
-                    }
-                }
-            }
-
-            //ss.SpeakTextAsync(text).GetAwaiter().GetResult();
-
-            //a.Speek().GetAwaiter().GetResult();
+            Current.MainWindow.Show();
 
         }
+
     }
 }
