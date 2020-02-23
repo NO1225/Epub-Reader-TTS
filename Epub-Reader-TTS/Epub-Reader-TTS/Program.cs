62f1353661b91461682962c90c2d399bbd643ee1
jumah safarty <safartyjumah@gmail.com>
2020-02-23 22:27:44 +0200

    before debugging dna

--- Epub-Reader-TTS/Epub-Reader-TTS/Program.cs
+++ Epub-Reader-TTS/Epub-Reader-TTS/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-
-namespace Epub_Reader_TTS
+﻿namespace Epub_Reader_TTS
 {
     public class Program
     {
