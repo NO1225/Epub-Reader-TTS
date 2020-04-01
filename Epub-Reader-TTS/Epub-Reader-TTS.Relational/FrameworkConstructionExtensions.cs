bf7ca86cf6ebac5c68b77709aad3af197f91104f
jumah safarty <safartyjumah@gmail.com>
2020-04-01 05:53:44 +0300

    Storing the database in the documents

--- Epub-Reader-TTS/Epub-Reader-TTS.Relational/FrameworkConstructionExtensions.cs
+++ Epub-Reader-TTS/Epub-Reader-TTS.Relational/FrameworkConstructionExtensions.cs
@@ -3,6 +3,8 @@ using Epub_Reader_TTS.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace Epub_Reader_TTS.Relational
 {
@@ -16,13 +18,24 @@ namespace Epub_Reader_TTS.Relational
         /// </summary>
         public static FrameworkConstruction AddClientDataStore(this FrameworkConstruction construction)
         {
-            var a = construction.Configuration.GetConnectionString("ClientDataStoreConnection");
+            // Getting the path to the document folder
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Epub Reader TTS");
+            
+            // Getting the current connection string
+            var tmpConnectionString = construction.Configuration.GetConnectionString("ClientDataStoreConnection");
+
+            // Check for preserved keys and replace them
+            var connectionString = tmpConnectionString.Replace("|Documents|", path);
+
+            // If there was any replacement, make sure the folder exist
+            if(tmpConnectionString != connectionString)
+                Directory.CreateDirectory(path);
 
             // Inject our SQLite EF data store
             construction.Services.AddDbContext<ClientDataStoreDbContext>(options =>
             {
                 // Setup connection string
-                options.UseSqlite(construction.Configuration.GetConnectionString("ClientDataStoreConnection"));
+                options.UseSqlite(connectionString);
             }, contextLifetime: ServiceLifetime.Transient);
 
             // Add client data store for easy access/use of the backing data store
